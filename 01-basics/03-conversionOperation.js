ea4f0ab88d2a939dfad44e149c922a1ab18145b4
Abhishekh <78847529+Abhishekh-creator@users.noreply.github.com>
2023-12-20 21:18:23 +0000

    conversion in operations done

--- 01-basics/03-conversionOperation.js
+++ 01-basics/03-conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "27"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 // 33 => 33
 // "33abc" = NaN
@@ -14,7 +14,7 @@ console.log(typeof valueInNumber);
 let isLoggedIn = 0;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -22,5 +22,27 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 27;
 let stringNumber = String(someNumber)
-console.log(someNumber)
-console.log(typeof stringNumber)
+// console.log(someNumber)
+// console.log(typeof stringNumber)
+
+// ************ Operations **************** \\
+
+let value = 3
+let negValue = -value
+//onsole.log(negValue)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+
+let numb1, numb2, numb3
+numb1 = numb2 = numb3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
