3bde8130d84d41b1f17ad3ad5f5da4b148ba5319
Abhishekh <78847529+Abhishekh-creator@users.noreply.github.com>
2023-12-21 15:04:09 +0000

    changes done

---
+++ 01-variable-dataTypes-methods-basics/03-conversionOperation.js
@@ -0,0 +1,48 @@
+let score = "27"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// 33 => 33
+// "33abc" = NaN
+//true => 1
+//false => 0
+
+let isLoggedIn = 0;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 27;
+let stringNumber = String(someNumber)
+// console.log(someNumber)
+// console.log(typeof stringNumber)
+
+// ************ Operations **************** \\
+
+let value = 3
+let negValue = -value
+//onsole.log(negValue)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+
+let numb1, numb2, numb3
+numb1 = numb2 = numb3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
