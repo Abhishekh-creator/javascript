6ca022fbe9d57853d5d1fe56e399ce53c9df2484
Abhishekh <78847529+Abhishekh-creator@users.noreply.github.com>
2023-12-22 21:08:11 +0000

    operators are done

---
+++ 01-basics/05-operator.js
@@ -0,0 +1,89 @@
+// 1: Airthmetic Operator
+let x 
+
+x = 5+5;
+x = 5*5;
+x = 5-5;
+x = 5/5;
+x = 5%5;
+
+// Concatenation => Concatenation: The + operator can also be used to put 2 or more strings together. The use of + depends on the data type
+x = 'Hello' + ' World';
+
+// Exponent => Exponentiation returns the result of the first operand to the power of the second.
+x = 2 ** 3
+
+// Increment is used to increment or add 1 to a value
+
+  x = 10;
+  x++; // 11
+
+//Decrement is used to decrement or remove 1 from a value
+  x = 10;
+  x--; 
+
+  // Assignment Operators
+  // Value assignment
+
+x = 10;
+//Addition assignment operator adds the amount on the right side
+x = 10;
+x += 10; // 20
+// Same as x = x + 10
+
+//Subtraction assignment operator subtracts the amount on the right side
+x = 10;
+x -= 10;  // 0
+// Same as x = x - 10
+
+//Multiplication assignment operator multiplies the amount on the right side
+x = 10;
+x *= 10; // 100
+// Same as x = x \* 10
+
+//Division assignment operator divides the amount on the right side
+x = 10;
+x /= 10; // 1
+// Same as x = x / 10
+
+//Modulo assignment operator gets division remainder of the amount on the right side
+x = 10;
+x %= 10; // 0
+// Same as x = x % 10
+
+//Exponentiation assignment operator gets exponent of the amount on the right side
+x = 10;
+x **= 10; // 100
+// Same as x = x \*\* 10
+console.log(x)
+
+//Comparison Operators
+// Comparison operators are used to compare values
+
+//Equal to operator - The following will return true even if the types do not match
+2 == 2 // true
+2 == '2' // true
+
+//Equal value & equal type - The types have to match or it will return false
+2 === 2 // true
+2 === '2' // false
+
+//Not equal to operator - returns true if not equal
+2 != 2 // false
+2 != '2' // false
+
+//Not equal value & equal type
+2 !== 2 // false
+2 !== '2' // true
+
+//Greater than
+10 > 5 // true
+
+//Less than
+10 < 5 // false
+
+//Greater than or equal
+10 >= 5 // true
+
+//Less than or equal
+10 <= 5 // false
